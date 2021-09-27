aee2512a622bf4b6fb0926cc7667d6bd0d99f9a7
ricardotenorio <ricardo.heber@gmail.com>
2021-09-27 08:40:43 -0300

    controllers e dialog box

--- lib/main.dart
+++ lib/main.dart
@@ -24,36 +24,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CadastroForm extends StatelessWidget {
+class CadastroForm extends StatefulWidget {
   const CadastroForm({Key? key}) : super(key: key);
+
+  @override
+  State<CadastroForm> createState() => _CadastroFormState();
+}
+
+class _CadastroFormState extends State<CadastroForm> {
+  final nomeController = TextEditingController();
+
+  final idadeController = TextEditingController();
+
+  final localizacaoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: TextField(
-            decoration: InputDecoration(
+            controller: nomeController,
+            decoration: const InputDecoration(
               border: UnderlineInputBorder(),
               hintText: 'Nome',
             ),
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: TextField(
-            decoration: InputDecoration(
+            controller: idadeController,
+            decoration: const InputDecoration(
               border: UnderlineInputBorder(),
               hintText: 'Idade',
             ),
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: TextField(
-            decoration: InputDecoration(
+            controller: localizacaoController,
+            decoration: const InputDecoration(
               border: UnderlineInputBorder(),
               hintText: 'Localização',
             ),
@@ -63,7 +77,7 @@ class CadastroForm extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(8, MediaQuery.of(context).size.height / 2, 8, 10),
           child:
             ElevatedButton(
-              onPressed: _cadastrar,
+              onPressed: () => _cadastrar(context),
               style: ElevatedButton.styleFrom(
                 primary: Colors.black12,
                 minimumSize: const Size.fromHeight(50)
@@ -75,5 +89,18 @@ class CadastroForm extends StatelessWidget {
     );
   }
 
-  void _cadastrar() {}
+  void _cadastrar(context) {
+    showDialog(
+      context: context, 
+      builder: (context) {
+      return AlertDialog(
+        content: Text(
+          'Nome: ${nomeController.text}\n'
+          'Idade: ${idadeController.text}\n'
+          'Localização: ${localizacaoController.text}',
+        ),
+      );
+      },
+    );
+  }
 }
